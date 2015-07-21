5afd3b6bbc746fde60d4bd5a4d3a95673e4b77fe
Sundeep <sunnyr22@gmail.com>
2015-07-20 22:16:19 -0400

    Almost done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,33 @@
 $(document).ready(function() {
-    $("li").click(function() {
+    $(".list-items").on("click", "li", function() {
     $(this).find(".item").toggleClass('stroked');
     });
+    $(window).keydown(function(event){
+	  if(event.keyCode == 13) {
+	  event.preventDefault();
+	  return false;
+    }
+  });
+
+// Adding new items
+	$(".itemInput").keydown(function(e) {
+		if (e.which === 13) {
+			addItem();
+			e.preventDefault();
+			return false;
+         }
+         
+     })
+	$(".list-items").on("click", ".remove",function(){
+			$(this).closest("li").remove();
+		})
+     $(".addToList").on("click", function() {
+     	 addItem();
+     });
+
 });
+
+function addItem(){
+	$(".list-items").append("<li><span class='item'>"+$(".itemInput").val()+"</span><img class='remove' src='images/x-mark.png'></li>");
+			$(".itemInput").val("");
+}
